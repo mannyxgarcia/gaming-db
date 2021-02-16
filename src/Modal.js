318a921d73449a938ef4e84e7804e83239842870
Manny Garcia <mannyalgarcia@gmail.com>
2021-02-16 17:56:18 -0500

    Feature- Added sliding carousel to each game category

--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 const Modal = ({ open, children, onClose }) => {
   if(!open) return null
   
-  return (
+  return ReactDOM.createPortal(
     <>
       <div className="modal-overlay" onClick={() => onClose()}/>
       <div className="modal">
         {children}
       </div>
-    </>
+    </>,
+    document.getElementById('portal')
   )
 }
 
--- src/SingleCategory.js
+++ src/SingleCategory.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import SingleGame from './SingleGame'
 import utils from './utils'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const SingleCategory = ({categoryName}) => {
   const [errors, setErrors] = useState(false)
@@ -10,7 +12,7 @@ const SingleCategory = ({categoryName}) => {
   
   const getGameDetails = (categoryName, date) => {
     const dates = utils.getDates(date)
-      fetch(`https://api.rawg.io/api/games?dates=${dates}&genres=${categoryName}&ordering=-rating&parent_platforms=1,2,3&page_size=5`)
+      fetch(`https://api.rawg.io/api/games?dates=${dates}&genres=${categoryName}&ordering=-rating&parent_platforms=1,2,3&page_size=10`)
       .then(res => res.json())
       .then(res => setGameDetails(res.results))
       .catch(() => setErrors(true))
@@ -22,14 +24,28 @@ const SingleCategory = ({categoryName}) => {
         <div className="category-title">
           {categoryName}
         </div>
-        <div className="category-games">
-          {gameDetails.map(game => {
-            return (
-              <div className="single-game">
-                <SingleGame key={game.id} id={game.id} gameDetails={game}/>
-              </div>
-            )
-          })}
+        <div className="container-carousel">
+        <CarouselProvider
+          naturalSlideWidth={10}
+          naturalSlideHeight={5}
+          totalSlides={5}
+          visibleSlides={2}>
+          <Slider>
+            <div className="category-games">
+              {gameDetails.map((game, index) => {
+                return (
+                  <Slide index={index}>
+                    <div className="single-game">
+                      <SingleGame key={game.id} id={game.id} gameDetails={game}/>
+                    </div>
+                  </Slide>
+                )
+              })}
+            </div>
+          </Slider>
+          <ButtonBack>Back</ButtonBack>
+          <ButtonNext>Next</ButtonNext>
+        </CarouselProvider>
         </div>
       </div>
     </>
